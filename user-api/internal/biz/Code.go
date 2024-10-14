4ecb835ecd2b6dbb4f1c642d266831bd50c059f3
lhw <434962239@qq.com>
2024-10-14 17:29:58 +0800

    完成用户注册接口

--- user-api/internal/biz/Code.go
+++ user-api/internal/biz/Code.go
@@ -1,10 +1,17 @@
 package biz
 
+import "github.com/zeromicro/x/errors"
+
 const (
 	Ok = 200
 )
 
+//var (
+//	DBError          = NewError(10000, "数据库错误")
+//	UserIsRegistered = NewError(10100, "用户已注册")
+//)
+
 var (
-	DBError          = NewError(10000, "数据库错误")
-	UserIsRegistered = NewError(10100, "用户已注册")
+	DBError          = errors.New(10000, "数据库错误")
+	UserIsRegistered = errors.New(10100, "用户已注册")
 )
--- user-api/internal/handler/account/registerhandler.go
+++ user-api/internal/handler/account/registerhandler.go
@@ -1,10 +1,9 @@
 package account
 
 import (
-	"net/http"
-	"user-api/internal/biz"
-
 	"github.com/zeromicro/go-zero/rest/httpx"
+	xhttp "github.com/zeromicro/x/http"
+	"net/http"
 	"user-api/internal/logic/account"
 	"user-api/internal/svc"
 	"user-api/internal/types"
@@ -14,16 +13,16 @@ func RegisterHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req types.RegisterReq
 		if err := httpx.Parse(r, &req); err != nil {
-			httpx.ErrorCtx(r.Context(), w, err)
+			xhttp.JsonBaseResponseCtx(r.Context(), w, err)
 			return
 		}
 
 		l := account.NewRegisterLogic(r.Context(), svcCtx)
 		resp, err := l.Register(&req)
 		if err != nil {
-			httpx.ErrorCtx(r.Context(), w, err)
+			xhttp.JsonBaseResponseCtx(r.Context(), w, err)
 		} else {
-			httpx.OkJsonCtx(r.Context(), w, biz.Success(resp))
+			xhttp.JsonBaseResponseCtx(r.Context(), w, resp)
 		}
 	}
 }
